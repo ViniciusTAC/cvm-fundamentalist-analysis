6a12a4d1de0f958c451493dedd4209169e6998cc
Vinícius Tadeu Andrade Costa <45463783+ViniciusTAC@users.noreply.github.com>
2025-01-26 17:37:01 -0300

    FEAT: Formulário de Referência cadastrando no banco (o repository tem um jeito novo para validar os data referencia documento)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+import os
+
 from datetime import datetime
 from collectors.coletor import Coletor
-from parsers.empresas_service import process_csv_files
-from storage.empresas_repository import ConexaoBanco
 
-import os
+# Empresas ---------------------------------------------------------------------------------------------------------------------
+# from parsers.empresas_service import process_csv_files
+# from storage.empresas_repository import ConexaoBanco
+
+from parsers.formulario_referencia_service import process_csv_files
+from storage.formulario_referencia_repository import ConexaoBanco
+
+
+# FRE ---------------------------------------------------------------------------------------------------------------------
 # from parsers.periodicos_eventuais_service import process_csv_files
 # from storage.periodicos_eventuais_repository import ConexaoBanco
 
@@ -11,26 +19,25 @@ def main():
     # # Instancia o coletor
     # coletor = Coletor()
 
-    # Inicia o processo de coleta de dados
+    # # Inicia o processo de coleta de dados
     # coletor.collect_data()
 
-    
-    base_path = os.path.join('data_extraido', 'FCA', 'sucesso')
-    empresas = process_csv_files(base_path)
+# Empresas ---------------------------------------------------------------------------------------------------------------------    
+    # base_path = os.path.join('data_extraido', 'FCA', 'sucesso')
+    # empresas = process_csv_files(base_path)
 
-    for i in range(3):
-        print(empresas[i].mostrarDados())
+    # for i in range(3):
+    #     print(empresas[i].mostrarDados())
 
-    # Conectar ao banco e salvar os dados
-    banco = ConexaoBanco(host='localhost', database='cvm_dados', user='root', password='31415')
+    # # Conectar ao banco e salvar os dados
+    # banco = ConexaoBanco(host='localhost', database='cvm_dados', user='root', password='31415')
 
-    banco.conectar()
-    for empresa in empresas:
-        banco.inserir_empresa(empresa)
-    banco.desconectar()
+    # banco.conectar()
+    # for empresa in empresas:
+    #     banco.inserir_empresa(empresa)
+    # banco.desconectar()
 
-
-    # base_path = os.path.join('data_extraido', 'IPE', 'sucesso')
+# IPE ---------------------------------------------------------------------------------------------------------------------
     # periodicos_eventuais = process_csv_files(base_path)
 
     # for i in range(3):
@@ -44,6 +51,21 @@ def main():
     # for periodico in periodicos_eventuais:
     #     banco.inserir_periodicos_eventuais(periodico)
     # banco.desconectar()
+    
+# FRE ---------------------------------------------------------------------------------------------------------------------
+    base_path = os.path.join('data_extraido', 'FRE', 'sucesso')
+    formulario_referencias = process_csv_files(base_path)
+
+    for i in range(3):
+        print(formulario_referencias[i].mostrarDados())
+
+    # Conectar ao banco e salvar os dados
+    banco = ConexaoBanco(host='localhost', database='cvm_dados', user='root', password='31415')
+
+    banco.conectar()
+    for formulario_referencia in formulario_referencias[:3]:
+        banco.inserir_formulario_referencia(formulario_referencia)
+    banco.desconectar()
 
 
 if __name__ == "__main__":
